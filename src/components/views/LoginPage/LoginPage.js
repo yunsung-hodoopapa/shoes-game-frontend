ed117ac78727f468a6e9ad60c2ecbe9c51982c1f
yunsung-hodoopapa <vappet3477@gamail.com>
2021-12-11 01:28:29 +0900

    change login logic

--- src/components/views/LoginPage/LoginPage.js
+++ src/components/views/LoginPage/LoginPage.js
@@ -1,13 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { withRouter } from 'react-router';
-<<<<<<< HEAD
 import { useDispatch, useSelector } from 'react-redux';
 import { loginUser } from '../../../actions/userAction';
-=======
-import { useDispatch } from 'react-redux';
-import { loginUser, refreshToken } from '../../../actions/userAction';
->>>>>>> master
 import SocialLogin from './SocialLogin/SocialLogin';
 import styled from 'styled-components';
 import axios from 'axios';
@@ -41,7 +36,6 @@ function LoginPage(props) {
     setPassword(e.currentTarget.value);
   };
 
-
   const onSubmitHandler = (e) => {
     e.preventDefault();
     const requestBody = {
@@ -50,7 +44,6 @@ function LoginPage(props) {
     };
     dispatch(loginUser(requestBody))
       .then((res) => {
-<<<<<<< HEAD
         console.log(res);
       if (res.payload.loginSuccess) {
         localStorage.setItem('userInfo', JSON.stringify(requestBody));
@@ -62,17 +55,6 @@ function LoginPage(props) {
     .catch((err) => {
       console.log(err);
     });
-=======
-        if (res.payload.loginSuccess) {
-          props.history.push('/main');
-        } else {
-          alert(res.payload.message);
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-      });
->>>>>>> master
   };
 
   // useEffect(() => {
